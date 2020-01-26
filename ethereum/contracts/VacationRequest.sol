79e05ba45d72e48200d4ccc353a40bf9219ce920
Konrad Mainzer <kmainzer@cloudeffect.net>
2020-01-26 13:24:02 +0000

    stat

--- ethereum/contracts/VacationRequest.sol
+++ ethereum/contracts/VacationRequest.sol
@@ -3,17 +3,17 @@ pragma solidity >=0.4.25 <0.7.0;
 contract VacationRequest
 {
     //Set of States
-    enum StateType { Request, Accepted, Rejected}
+    enum StateType { Draft, PendingApproval, Accepted, Rejected, Cancelled}
 
     //List of properties
     StateType public  State;
-    address public  Requestor;
-    address public  Responder;
+    address public  Requestor; // IO
+    address public  Responder; // IM
 
     // Constructor
     constructor () public
     {
         Requestor = msg.sender;
-        State = StateType.Request;
+        State = StateType.Draft;
     }
 }
