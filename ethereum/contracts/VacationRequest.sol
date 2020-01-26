46d251588b52e07188d74c56323cd2091de0e430
Konrad Mainzer <kmainzer@cloudeffect.net>
2020-01-26 13:20:04 +0000

    Vacation request

--- ethereum/contracts/VacationRequest.sol
+++ ethereum/contracts/VacationRequest.sol
@@ -1,1 +1,19 @@
-pragma solidity >=0.4.22 <0.7.0;
+pragma solidity >=0.4.25 <0.7.0;
+
+contract VacationRequest
+{
+    //Set of States
+    enum StateType { Request, Accepted, Rejected}
+
+    //List of properties
+    StateType public  State;
+    address public  Requestor;
+    address public  Responder;
+
+    // Constructor
+    constructor () public
+    {
+        Requestor = msg.sender;
+        State = StateType.Request;
+    }
+}
--- ethereum/migrations/2_deploy_contracts.js
+++ ethereum/migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
 const ConvertLib = artifacts.require("ConvertLib");
 const MetaCoin = artifacts.require("MetaCoin");
+const VacationRequest = artifacts.require("VacationRequest");
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
+
+  deployer.deploy(VacationRequest);
 };
