03a797d21ec01fedac329282319459bc1b6d83c6
Konrad Mainzer <kmainzer@cloudeffect.net>
2020-01-28 09:51:57 +0000

    Added a few test, submit and approve

--- ethereum/test/vacationRequest.js
+++ ethereum/test/vacationRequest.js
@@ -37,7 +37,6 @@ contract('VacationRequest', function (accounts) {
       throw null;
     } catch (error) {
       assert(error, "Expected an error but did not get one");
-
       var message = "revert Only Status Pending Approval can be rejected";
       assert(error.message.startsWith(PREFIX + message), "Expected " + message + ". But got " + error.message);
     }
@@ -51,8 +50,33 @@ contract('VacationRequest', function (accounts) {
       throw null;
     } catch (error) {
       assert(error, "Expected an error but did not get one");
-
       var message = "revert Only Status Pending Approval can be rejected";
+      assert(error.message.startsWith(PREFIX + message), "Expected " + message + ". But got " + error.message);
+    }
+  });
+
+  it("Cannot Approve, only manager can approve", async function () {
+    const instance = await VacationRequest.new();
+    try {
+      await instance.upsertDay({ year: 2020, month: 2, day: 20, amount: 8 });
+      await instance.submit('0x7E9F14fB93C172ce09D15186f1E4BC5aB2a522b0');
+      await instance.approve();
+    } catch (error) {
+      assert(error, "Expected an error but did not get one");
+      var message = "revert Only manager can use this function";
+      assert(error.message.startsWith(PREFIX + message), "Expected " + message + ". But got " + error.message);
+    }
+  });
+
+  it("Cannot Submit request to yourself", async function () {
+    const instance = await VacationRequest.new();
+    try {
+      await instance.upsertDay({ year: 2020, month: 2, day: 20, amount: 8 });
+      const owner = await instance.owner();
+      await instance.submit(owner);
+    } catch (error) {
+      assert(error, "Expected an error but did not get one");
+      var message = "revert Cannot submit request to yourself";
       assert(error.message.startsWith(PREFIX + message), "Expected " + message + ". But got " + error.message);
     }
   });
