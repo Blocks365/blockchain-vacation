9bfe4b1bdc668e677e15f78ca7a4290ac7f1438b
Yashar Ghouchibeik <y.ghouchibeik@orkestratie.nl>
2020-02-17 11:56:03 +0100

    adding the Migration.sol

--- ethereum/migrations/2_deploy_contracts.js
+++ ethereum/migrations/2_deploy_contracts.js
@@ -8,7 +8,7 @@ module.exports = function(deployer) {
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
 
-  //deployer.deploy(VacationManager);
-  //deployer.link(VacationManager, VacationRequest);
-  //deployer.deploy(VacationRequest, 0xe9ae1C5A17c632e22d8BBAd39d7F754FdDA4e8B9);  
+  deployer.deploy(VacationManager);
+  deployer.link(VacationManager, VacationRequest);
+  deployer.deploy(VacationRequest, 0xe9ae1C5A17c632e22d8BBAd39d7F754FdDA4e8B9);  
 };
